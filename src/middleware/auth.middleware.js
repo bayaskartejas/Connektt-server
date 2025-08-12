f17adcd9a4de61d3260d741a5533b5e49c9074a5
Tejas Bayaskar <tejasbayaskar8506@gmail.com>
2025-08-12 22:37:58 +0530

    JWT key test

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,6 +4,8 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 export function authenticate(req, res, next) {
   const authHeader = req.headers.authorization;
+  console.log("JWT Key is: " + JWT_SECRET);
+  
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ error: 'No token provided' });
